65a21ba08db418ba1727fd758d68c81d41523b86
Zakkkk <zak.g.venter@gmail.com>
2023-10-08 22:46:13 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,44 @@
+from sample import *
+from utils import *
 from functions import *
-from function_information import *
-import random
-
-def create_sample(size, function):
-    collection = []
-    for i in range(size):
-        collection.append(function(random.randint(1,100)))
-    
-    return find_sample(collection)
+from numpy import std
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.optimize import curve_fit
+import math
 
 sample_size = int(input('Sample size: '))
+number_of_samples = int(input('Number of samples to be generated: '))
 
-samples_collection = []
-for i in range(1000000):
-    samples_collection.append(create_sample(sample_size, linear)[0]) # choose function type here
+data = sort_into_categories(find_sample_collection(sample_size, number_of_samples, linear)[0])
 
-samples_mean, samples_std_dev = find_sample(samples_collection)
+x = np.arange(1, len(data) + 1)
+y = np.array(data)
 
-true_mean, true_std_dev = find_true(linear)
+# Create x and y arrays from the given data
+x = np.arange(1, len(data) + 1)
+y = np.array(data)
 
-print(f"The mean is expected to be {true_mean} and the std dev is expected to be {true_std_dev/math.sqrt(sample_size)}\n")
+# Define the Gaussian function for the line of best fit
+def gaussian_fit(x, m, s):
+    return 1 / (s * math.sqrt(2 * math.pi)) * np.exp(-1 / 2 * ((x - m) / s)**2)
 
-print(f"The actual mean is {samples_mean} and the actual std dev is {samples_std_dev}\n")
+# Fit the data to the Gaussian function
+params, _ = curve_fit(gaussian_fit, x, y)
+
+# Extract the fitted parameters
+m, s = params
+
+# Generate the predicted y values for the line of best fit
+y_fit = gaussian_fit(x, m, s)
+
+# Create the matplotlib plot
+plt.figure(figsize=(10, 6))
+plt.plot(x, y, label='Data')
+plt.plot(x, y_fit, label=f'Line of Best Fit: Gaussian', color='red')
+plt.xlabel('x')
+plt.ylabel('y')
+plt.title('Data and Line of Best Fit (Gaussian)')
+plt.legend()
+plt.grid(True)
+plt.show()
