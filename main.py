ce0c688b95ada5424b79f9f2d46756671a9825ea
Zakkkk <zak.g.venter@gmail.com>
2023-10-11 02:51:14 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,44 +1,65 @@
-from sample import *
-from utils import *
 from functions import *
-from numpy import std
+from findError import find_collection_error
 import matplotlib.pyplot as plt
-import numpy as np
-from scipy.optimize import curve_fit
-import math
+from utils import human_format
 
-sample_size = int(input('Sample size: '))
+while True:
+    option_input = ''
+    print("Choose a function type:")
+    options = [
+        ['linear', linear],
+        ['exponential', exponential], 
+        ['sinusoidal', sinusoidal], 
+        ['absolute', absolute], 
+        ['ellipse', ellipse], 
+        ['upside down normal', upside_normal],
+        ['quadratic', quadratic]
+    ]
+    for i, option in enumerate(options):
+        print(f'{i}: {option[0]}')
+    option_input = input()
+    try:
+        function_name = options[int(option_input)][0]
+        chosen_function = options[int(option_input)][1]
+        break
+    except:
+        print('try again.')
+
 number_of_samples = int(input('Number of samples to be generated: '))
 
-data = sort_into_categories(find_sample_collection(sample_size, number_of_samples, linear)[0])
+single_or_multiple = int(input('Single trial or series? (S: 0, M: 1): '))
+if single_or_multiple == 0: # single trial
+    sample_size = int(input('Sample size: '))
+    find_collection_error(
+        chosen_function,
+        sample_size, 
+        number_of_samples,
+        plot_graph=True)
+    
+else: # mutliple trials
+    upper_limit = int(input('Upper limit n: '))+1
+    list_of_err = []
 
-x = np.arange(1, len(data) + 1)
-y = np.array(data)
+    for i in range(1, upper_limit):
+        list_of_err.append(
+            find_collection_error(
+                chosen_function,
+                i, 
+                number_of_samples)
+        )
+        
+        print(f'{i}/{upper_limit-1} completed...')
 
-# Create x and y arrays from the given data
-x = np.arange(1, len(data) + 1)
-y = np.array(data)
+    for i, err in enumerate(list_of_err):
+        print(f'n={i+1}: P={round(err * 100, 2)}%')
+    
+    x = [i for i in range(1,upper_limit)]
+    y = [100 * j for j in list_of_err]
 
-# Define the Gaussian function for the line of best fit
-def gaussian_fit(x, m, s):
-    return 1 / (s * math.sqrt(2 * math.pi)) * np.exp(-1 / 2 * ((x - m) / s)**2)
-
-# Fit the data to the Gaussian function
-params, _ = curve_fit(gaussian_fit, x, y)
-
-# Extract the fitted parameters
-m, s = params
-
-# Generate the predicted y values for the line of best fit
-y_fit = gaussian_fit(x, m, s)
-
-# Create the matplotlib plot
-plt.figure(figsize=(10, 6))
-plt.plot(x, y, label='Data')
-plt.plot(x, y_fit, label=f'Line of Best Fit: Gaussian', color='red')
-plt.xlabel('x')
-plt.ylabel('y')
-plt.title('Data and Line of Best Fit (Gaussian)')
-plt.legend()
-plt.grid(True)
-plt.show()
+    plt.figure(figsize=(8, 5)) 
+    plt.plot(x, y)
+    plt.xlabel('n')
+    plt.ylabel('% error')
+    plt.title(f'% error based on n ({function_name}), {human_format(number_of_samples)} samples')
+    plt.grid(True)
+    plt.show()
