2253b1157f89d30bab0adeeb8041a266f185bca0
Zakkkk <zak.g.venter@gmail.com>
2023-10-11 02:50:22 +0800

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,17 +1,18 @@
 import random
-from utils import find_mean
+from utils import find_mean, progressBar
 from numpy import std
 
 def find_sample_mean(size, function):
     collection = []
     for i in range(size):
-        collection.append(function(random.randint(0,100)+1))
+        collection.append(function(random.randint(0,99)))
     
     return find_mean(collection)
 
 def find_sample_collection(sample_size, number_of_samples, function):
     samples_collection = []
-    for i in range(number_of_samples):
-        samples_collection.append(find_sample_mean(sample_size, function))
+    for i in progressBar(range(number_of_samples), prefix = 'Progress:', suffix = 'Complete', length = 50):
+        samples_collection.append(find_sample_mean(sample_size, function)) 
+        #perhaps divide sample mean bt number of samples
 
     return samples_collection, find_mean(samples_collection), std(samples_collection)
